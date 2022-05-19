278da38b1ae75d07c08bcc2c0180cdd6f6130e10
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-05-18 20:23:17 -0400

    Add post-route api log

---
+++ routes/api/post-routes.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const { Post, User } = require('../../models');
+
+// get all users
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+      // Query configuration
+    })
+  
+  });
+
+  Post.findAll({
+    attributes: ['id', 'post_url', 'title', 'created_at'],
+  })
+
+  include: [
+    {
+      model: User,
+      attributes: ['username']
+    }
+  ]
+
+  // get all users
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: ['id', 'post_url', 'title', 'created_at'],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  module.exports = router;
